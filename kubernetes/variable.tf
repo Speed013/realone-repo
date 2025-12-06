fbc29750320b48158fc9c6fb74d0725ed8e59c05
tkibnyusuf <85669533+tkibnyusuf@users.noreply.github.com>
2025-12-06 13:11:39 -0600

    Add files via upload

---
+++ kubernetes/variable.tf
@@ -0,0 +1,10 @@
+variable vpc_cidr_block {}
+variable private_subnet_cidr_blocks {}
+variable public_subnet_cidr_blocks {}
+variable vpc_name {}
+variable cluster_vs {}
+variable name_of_eks_cluster {}
+variable env_name {}
+variable region {}
+
+
