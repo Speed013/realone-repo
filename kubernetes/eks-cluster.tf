4d6070eeb811598c3ea36c56a2b23ca0566cb44f
Speed013 <alidosunmu@gmail.com>
2025-12-24 13:26:46 -0800

    Update eks-cluster.tf

--- kubernetes/eks-cluster.tf
+++ kubernetes/eks-cluster.tf
@@ -1,46 +1,38 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 21.0"
-
-  name               = var.name_of_eks_cluster
-  kubernetes_version = var.cluster_vs
-
-  addons = {
-    coredns                = {}
-    eks-pod-identity-agent = {
-      before_compute = true
+  version = "~> 19.0"
+  cluster_endpoint_public_access  = true
+  cluster_addons = {
+    coredns = {
+      most_recent = true
     }
-    kube-proxy             = {}
-    vpc-cni                = {
-      before_compute = true
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
     }
   }
+  
 
-  # Optional
-  endpoint_public_access = true
-
-  # Optional: Adds the current caller identity as an administrator via cluster access entry
-  enable_cluster_creator_admin_permissions = true
-
+  cluster_name = "myAppp-eks-cluster"  
+  cluster_version = "1.33"
   subnet_ids = module.myAppp-vpc.private_subnets
   vpc_id = module.myAppp-vpc.vpc_id
-  control_plane_subnet_ids = module.myAppp-vpc.private_subnets
 
-  # EKS Managed Node Group(s)
+  tags = {
+    environment = "development"
+    application = "myAppp"
+  }
+
   eks_managed_node_groups = {
-    example = {
-      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
-      ami_type       = "AL2023_x86_64_STANDARD"
-      instance_types = ["t2.small"]
-
+    dev = {
       min_size     = 1
       max_size     = 6
       desired_size = 3
-    }
-  }
 
-  tags = {
-    environment = var.env_name
-    application = "myAppp"
+      instance_types = ["t2.small"]
+      key_name       = "tnk"
+    }
   }
 }
